b6139ea5e83494a5b145194473cdf4983ef286ce
dahyun0917 <dahyun0917@naver.com>
2022-10-05 00:56:52 +0900

    로그인 여부를 나타내는 툴바

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -8,6 +8,7 @@ import CommentList from './chapter_05/CommentList';
 import NotificationList from './chapter_06/NotificationList';
 import Accommodate from './chapter_07/Accommodate';
 import ConfirmButton from './chapter_08/ConfirmButton';
+import LandingPage from './chapter_09/LandingPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -37,9 +38,16 @@ setInterval(() => {
   // );
 
   // 8챕터
+  // root.render(
+  //   <React.StrictMode>
+  //     <ConfirmButton />
+  //   </React.StrictMode>
+  // );
+
+  // 9챕터
   root.render(
     <React.StrictMode>
-      <ConfirmButton />
+      <LandingPage />
     </React.StrictMode>
   );
 },1000)
